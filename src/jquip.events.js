f68920281f872ad7029e7ebab482c82beb8c7e76
Alexey Neretin <alexey.neretin@webspellchecker.net>
2016-04-21 12:54:25 +0300

    Fixed focus and blur events triggering for nonfocusable nodes(issue with failing tests via karma)

--- src/jquip.events.js
+++ src/jquip.events.js
@@ -135,7 +135,7 @@ $['plug']("events", function($){
   };
     p['trigger'] = function (evt) {
         return this['each'](function () {
-            if ((evt == "click" || evt == "blur" || evt == "focus") && this[evt])
+            if ((evt == "click") && this[evt])
                 return this[evt]();
             if (doc.createEvent) {
                 var e = doc.createEvent('Events');
